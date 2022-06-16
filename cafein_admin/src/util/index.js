0c26a0ab47a3511860016f5ae109f1ef1d44d030
UFFETKK <88070276+UFFETKK@users.noreply.github.com>
2022-06-16 16:06:32 +0900

    Update index.js

--- cafein_admin/src/util/index.js
+++ cafein_admin/src/util/index.js
@@ -4,6 +4,7 @@ import { setInterceptors } from "./interceptors";
 function withAuth() {
   const instance = axios.create({
     baseURL: process.env.REACT_APP_API_URL + "/admin",
+    withCredentials: true,
   });
   return setInterceptors(instance);
 }
